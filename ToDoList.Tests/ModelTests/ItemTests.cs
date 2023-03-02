0a70a885c813fbe696ca4063f32378807d62fecc
Zachary <ZackCipolletta@gmail.com>
2023-03-01 21:44:47 -0800

    complete all wednesday reading.

--- ToDoList.Tests/ModelTests/ItemTests.cs
+++ ToDoList.Tests/ModelTests/ItemTests.cs
@@ -80,5 +80,53 @@ namespace ToDoList.Tests
       //Assert
       CollectionAssert.AreEqual(newList, result);
     }
+
+    [TestMethod]
+    public void GetId_ItemsInstatiateWithAnIdAndGetterReturns_Int()
+    {
+      //Arrange
+      string description = "Walk the dog.";
+      Item newItem = new Item(description);
+
+      //Act
+      int result = newItem.Id;
+
+      //Assert
+      Assert.AreEqual(1, result);
+    }
+
+    [TestMethod]
+    public void Find_ReturnsCorrectItem_Item()
+    {
+      //Arrange
+      string description01 = "Walk the dog";
+      string description02 = "Wash the dishes";
+      Item newItem1 = new Item(description01);
+      Item newItem2 = new Item(description02);
+
+      // Act
+      Item result = Item.Find(2);
+
+      // Assert
+      Assert.AreEqual(newItem2, result);
+    }
+
+      [TestMethod]
+  public void Find_ReturnsCorrectCategory_Category()
+  {
+    //Arrange
+    string name01 = "Work";
+    string name02 = "School";
+    Category newCategory1 = new Category(name01);
+    Category newCategory2 = new Category(name02);
+
+    //Act
+    Category result = Category.Find(2);
+
+    //Assert
+    Assert.AreEqual(newCategory2, result);
   }
+
+  }
+  
 }
