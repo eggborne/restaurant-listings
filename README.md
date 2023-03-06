113466be33229278a22eb1e331639af62b20c2cc
Zachary <ZackCipolletta@gmail.com>
2023-03-05 18:06:49 -0800

    add abililty to connect to MySQL workbench

--- README.md
+++ README.md
@@ -11,6 +11,19 @@ There are multiple branches in this repo that are described more below.
 3. Run `dotnet watch run` in the command line to start the project in development mode with a watcher.
 4. Open the browser to _https://localhost:5001_. If you cannot access localhost:5001 it is likely because you have not configured a .NET developer security certificate for HTTPS. To learn about this, review this lesson: [Redirecting to HTTPS and Issuing a Security Certificate](https://www.learnhowtoprogram.com/c-and-net/basic-web-applications/redirecting-to-https-and-issuing-a-security-certificate).
 
+
+Creating a Database Connection String
+When we connect to our database, we must manually open a connection by creating a new instance of the MySqlConnection class from the MySqlConnector package. We'll learn exactly how to do this in upcoming lessons. What's important to know right now is that the MySqlConnection constructor expects a database connection string that contains the access details of our database: its name, the database user and password, and the database server and port. For our To Do List app, this is what our connection string will look like:
+
+Next, let's create appsettings.json. Within the production directory of our project (ToDoList.Solution/ToDoList), create appsettings.json and add the following code to it:
+{
+    "ConnectionStrings": {
+        "DefaultConnection": "Server=localhost;Port=3306;database=to_do_list_with_mysqlconnector;uid=[YOUR-USERNAME-HERE];pwd=[YOUR-PASSWORD-HERE];"
+    }
+}
+Make sure to update the connection string with the values that you set for your username and password. As noted earlier, we'll always assume the user is 'root' and the password is epicodus.
+
+
 ## Available Branches
 
 **1_mvc_setup**: this is the default branch with the starter code for the To Do List project as an ASP.NET Core MVC web application. The walkthrough of this example project starts in this lesson:
--- ToDoList/Program.cs
+++ ToDoList/Program.cs
@@ -7,21 +7,57 @@
 
 //namespace ToDoList
 // {
-  //  if a file has multiple classes in it, the namespace statement will apply to all classes. This means that we cannot use namespace statements when a single file has multiple namespaces in it.
+//  if a file has multiple classes in it, the namespace statement will apply to all classes. This means that we cannot use namespace statements when a single file has multiple namespaces in it.
 // namespace ToDoList;
 
 // class Program << ---These are also not necessary
 // {
 //     static void Main(string[] args) << --- These are also not necessary
 //     {  Note that we do not have to name our the entry point file of our program Program.cs. However, for top level statements to work, we can only use it in one file. This includes console applications as well as ASP.NET Core applications. To learn more about top level statements visit the MS Documentation on top level statements.
+// WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+// builder.Services.AddControllersWithViews();
+
+// DBConfiguration.ConnectionString = builder.Configuration["ConnectionStrings:DefaultConnection"];
+
+// WebApplication app = builder.Build();
+
+// app.UseHttpsRedirection();
+// app.UseStaticFiles();
+// app.UseRouting();
+
+// app.MapControllerRoute(
+//   name: "default",
+//   pattern: "{controller=Home}/{action=Index}/{id?}"
+// );
+
+// app.Run();
+//   }
+// }
+// }
+
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using ToDoList.Models;
+
+namespace ToDoList
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
       WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
       builder.Services.AddControllersWithViews();
 
+      DBConfiguration.ConnectionString = builder.Configuration["ConnectionStrings:DefaultConnection"];
+
       WebApplication app = builder.Build();
 
+    //   app.UseDeveloperExceptionPage();
       app.UseHttpsRedirection();
       app.UseStaticFiles();
+
       app.UseRouting();
 
       app.MapControllerRoute(
@@ -30,6 +66,6 @@
       );
 
       app.Run();
-  //   }
-  // }
-// }
+    }
+  }
+}
